751776f0e461cf5c47daa082129b8b4b75e72270
Arjun Bharadwaj <m.arjunbharadwaj@gmail.com>
2025-03-31 16:35:28 -0700

    Project Set up

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ def create_app():
 
     app = Flask(__name__)
 
+    app.config.from_prefixed_env()      #This will load up environmental variables from the .env file with prefix
+    # default prefix is FLASK prefix
     return app
